0e7d08b07e872cb6d29ed48eb85d6ee2a788987d
Stanislav Beresnev <s.beresnev@vk.team>
2026-02-23 21:22:55 +0300

    feat: корректироква DefineModelRepositoryError

--- src/Euclid/errors.ts
+++ src/Euclid/errors.ts
@@ -1,4 +1,10 @@
+import { i18n } from "../utils/i18n";
+
 export class DefineModelRepositoryError extends Error {
   code: string = "001";
   name: string = "DefineModelRepositoryError";
+  constructor(repository: string) {
+    super();
+    this.message = i18n(`Репозиторий ${repository} не является допустимым`);
+  }
 }
---
+++ src/utils/i18n.ts
@@ -0,0 +1,8 @@
+/**
+ * Временная реализация i18n
+ * @param message локализуемая строка
+ * @returns локализованная
+ */
+export function i18n(message: string) {
+  return message;
+}
